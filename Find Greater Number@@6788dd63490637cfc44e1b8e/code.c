65928fb6b009defbe17a6b73dd98ddb0e5d297e7
Bhoomika201 <bhoomika0201.becse24@chitkara.edu.in>
2025-01-28 22:03:55 +0530

    Solution to Find Greater Number

---
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf(" ",a,b);
+    scanf("%d%d",&a,&b);
+    if(a>b){
+        printf("%d",a);
+    }
+    else{
+        printf("%d",b);
+    }
+    return 0;
+}
