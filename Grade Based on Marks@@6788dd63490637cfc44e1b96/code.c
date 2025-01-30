28640fbf24841f0fa1430b803a9f24bf7be31430
Bhoomika201 <bhoomika0201.becse24@chitkara.edu.in>
2025-01-30 21:08:47 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int marks;
+    printf(" ",marks);
+    scanf("%d",&marks);
+    if(marks>=90){
+        printf("A");
+    }
+    else if(90>marks>=80){
+        printf("B");
+    }
+    else if(80>marks>=70){
+        printf("C");
+    }
+    else if(70>marks>=60){
+        printf("D");
+    }
+    else{
+        printf("F");
+    }
+    return 0;
+}
