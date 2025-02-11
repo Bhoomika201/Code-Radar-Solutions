3e7c118da643a5cbe53ffa53d38773fdc0781eb1
Bhoomika201 <bhoomika0201.becse24@chitkara.edu.in>
2025-02-11 14:44:42 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,23 @@
+// Your code here...
+#include <stdio.h>
+int main(){
+    char ch;
+    if(ch == 'A'){
+        printf("Excellent");
+    }
+    else if(ch == 'B'){
+        printf("Good");
+    }
+    else if(ch == 'C'){
+        printf("Average");
+    }
+    else if(ch == 'D'){
+        printf("below Average");
+    }
+    else if(ch=='F'){
+        printf("Fail");
+    }
+    else{
+        printf("");
+    }
+}
